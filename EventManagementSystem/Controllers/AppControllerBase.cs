05073c07886fc72ebb899399a57a98dadd598b4a
Shukridozom <Shukridozom@hotmail.com>
2023-12-04 19:02:09 +0300

    Created an Application base controller to encapsulate all common logic between application API controllers [ all Application API Controllers will inherit from it ]

---
+++ EventManagementSystem/Controllers/AppControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppControllerBase : ControllerBase
+    {
+        protected readonly AppDbContext context;
+        protected readonly IConfiguration config;
+        protected readonly IMapper mapper;
+
+        public AppControllerBase(AppDbContext context, IConfiguration config, IMapper mapper)
+        {
+            this.context = context;
+            this.config = config;
+            this.mapper = mapper;
+        }
+    }
+}
