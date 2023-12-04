7aeab6b1bbd8a14e6cddf754fe46ed0625a95604
Shukridozom <Shukridozom@hotmail.com>
2023-12-04 20:57:00 +0300

    Edited the characterset allowed for passwords

--- EventManagementSystem/CustomDataAnnotations/PasswordCharacterSet.cs
+++ EventManagementSystem/CustomDataAnnotations/PasswordCharacterSet.cs
@@ -4,7 +4,7 @@ namespace EventManagementSystem.CustomDataAnnotations
 {
     public class PasswordCharacterSet : ValidationAttribute
     {
-        string passwordCharacterSet = "abcdefghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789_.*@#$%!?";
+        string passwordCharacterSet = @"abcdefghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()_-+={[}]|\:;""'<,>.?/";
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             var password = value?.ToString() ?? string.Empty;
@@ -15,7 +15,7 @@ namespace EventManagementSystem.CustomDataAnnotations
             foreach (var character in password)
             {
                 if (!passwordCharacterSet.Contains(character))
-                    return new ValidationResult("\"Allowed characters: [a...z][A...Z][0...9] and these symbols: _.*@#$%!?");
+                    return new ValidationResult(@"Allowed characters: [a...z][A...Z][0...9] and these symbols: ~`!@#$%^&*()_-+={[}]|\:;""'<,>.?/");
             }
             return ValidationResult.Success;
         }
