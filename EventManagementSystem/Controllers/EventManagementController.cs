02044e9fd62c709c7686152665e1efad2c78dbbd
Shukridozom <Shukridozom@hotmail.com>
2023-12-05 20:33:43 +0300

    Created an API Controller for managing events [update - delete events]

---
+++ EventManagementSystem/Controllers/EventManagementController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using EventManagementSystem.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EventManagementController : AppControllerBase
+    {
+        public EventManagementController(AppDbContext context, IConfiguration config, IMapper mapper)
+            : base(context, config, mapper)
+        {
+
+        }
+
+        [HttpPut]
+        [Route("/api/events/{id}")]
+        public IActionResult EditEvent(int id, CreateEventDto eventDto)
+        {
+            var eventFromDb = context.Events.SingleOrDefault(e => e.Id == id);
+            var userId = GetUserId();
+
+            if (eventFromDb == null)
+                return NotFound();
+
+            if (eventFromDb.UserId != userId)
+                return NotFound();
+
+            mapper.Map(eventDto, eventFromDb);
+
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("/api/events/{id}")]
+        public IActionResult DeleteEvent(int id)
+        {
+            var userId = GetUserId();
+            var eventFromDb = context.Events.SingleOrDefault(e => e.Id == id);
+            if (eventFromDb == null)
+                return NotFound();
+
+            if (eventFromDb.UserId != userId)
+                return NotFound();
+
+            context.Events.Remove(eventFromDb);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
