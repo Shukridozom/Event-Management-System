4ceda3abd44666d0b8b48ee6567d5cdd7ab4e24c
Shukridozom <Shukridozom@hotmail.com>
2023-12-06 03:50:20 +0300

    Configured the Pipline to use Swagger with both Development & Production

--- EventManagementSystem/Program.cs
+++ EventManagementSystem/Program.cs
@@ -41,7 +41,7 @@ namespace EventManagementSystem
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
